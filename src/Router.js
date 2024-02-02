7c6380557cec43153aee46f5607699dcf6f4babb
lexeach <80155340+lexeach@users.noreply.github.com>
2024-02-02 18:01:18 +0530

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -8,9 +8,9 @@ function Router() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<App />} /> 
+        {/* <Route path="/" element={<App />} /> */}
         <Route path="/" element={<Purchaser />} /> 
-       {/* <Route path="/QR" element={<QR />} /> */}
+        <Route path="/QR" element={<QR />} /> 
         
       </Routes>
     </div>
